84fd398a9f50b2eb1f6c6512d10930c84f7ed8ba
Erick Lopez <xerick_cid@outlook.com>
2025-03-11 22:06:13 -0600

    Add files via upload

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Arreglos_Nombres{
+    String[] Nombres = new string[5];
+    public void Nombres_En_Arreglos(){
+        for (int i = 0; i < 5; i++){
+                Console.WriteLine($"Ingresa el nombre numero {i + 1}");
+                Nombres[i] = Console.ReadLine();
+        }
+    }
+    public void Arreglo_Y_Posicion(){
+        int Posicino_Encontrada = -1;
+        Console.WriteLine("Ingresa el nombre que desee buscar");
+        string Nombre_Encontrado = Console.ReadLine();
+        for (int i = 0; i < 5; i++){
+            if (string.Equals(Nombres[i], Nombre_Encontrado, StringComparison.OrdinalIgnoreCase)){
+                Posicino_Encontrada = i;
+                break;
+            }
+        }
+        Console.WriteLine($"El nombre que buscaba se encontro en la posicion {Posicino_Encontrada}");
+    }
+}
+
+class Ejercicio_2{
+    static void Main(){
+        String[] Nombres = new String[5];
+        Arreglos_Nombres Nuevo_Nombre = new Arreglos_Nombres();
+        Nuevo_Nombre.Nombres_En_Arreglos();
+        Nuevo_Nombre.Arreglo_Y_Posicion();
+    }
+}
+
+//Solicita al usuario llenar un arreglo de 5 nombres y luego pedirle un  nombre a buscar.
+//El programa debe indicar si el nombre esta en el arreglo y en que posicion se encuentra
---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Ordenamiento_De_Numeros{
+    public void Orden_De_Numeros(){
+        Console.WriteLine("Cuantos numeros desea ingresar?");
+        int Cantidad_De_Numeros = int.Parse(Console.ReadLine());
+        int[] Numeros = new int[Cantidad_De_Numeros];
+
+        Console.WriteLine("Ingresa los numeros");
+        for (int i = 0 ; i < Cantidad_De_Numeros ; i++ ){
+            Console.WriteLine($"Numero {i + 1}: ");
+            Numeros[i] = int.Parse(Console.ReadLine()); 
+        }
+
+        int Longitud_De_Arreglo = Numeros.Length;
+        bool Intercambio_Realizado;
+        for (int i = 0 ; i < Longitud_De_Arreglo - 1 ; i++){ //Se utiliza una condicional donde se le resta -1 a la segunda variable para que no busque un arreglo inexistente (Si tienes 5 posiciones de arreglos que es del 0 al 4. No buscara la posicion 5)
+            Intercambio_Realizado = false;
+            for (int j = 0 ; j < Longitud_De_Arreglo -i -1 ; j++){ //Se hace lo mismo pero restandole el valor de i para que este disminuya a la par.
+                if(Numeros[j] > Numeros[j + 1]){ //Aqui utilizamos "Numeros" como condicional ya que necesitamos el arreglo y no la longitud de este
+                    int Temporal = Numeros[j]; //El valor del primer arreglo se pasa a la variable temporal
+                    Numeros[j] = Numeros[j + 1];//El valor del segundo arreglo cambia al arreglo numero 1
+                    Numeros[j + 1] = Temporal;//El valor del segundo arreglo para a la variable temporal
+                    Intercambio_Realizado = true;
+                }
+            } // Utilizando 2 for estariamos comparando la posicion x con la siguiente posicion
+            if (!Intercambio_Realizado){
+                break;
+            }
+        }
+        Console.WriteLine("Orden de los numeros de menor a mayor.");
+        for (int i = 0 ; i < Longitud_De_Arreglo ; i++ ){
+            Console.WriteLine(Numeros[i]);
+        }
+    }
+}
+class Ejercicio_3{
+    static void Main(){
+        Ordenamiento_De_Numeros Lista_De_Numeros = new Ordenamiento_De_Numeros();
+        Lista_De_Numeros.Orden_De_Numeros();
+    }
+}
+
+/*
+Implementa el metodo de ordenamiento burbuja para ordenar un arreglo de numeros
+ingresados por el usuario de menor a mayor
+*/
